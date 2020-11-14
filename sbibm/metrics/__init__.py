28d2ca8e15850cf5b5d4fa236aa48433791404fa
Jan-Matthis <mail@jan-matthis.de>
2020-11-14 12:54:07 +0100

    Rename metrics

--- sbibm/metrics/__init__.py
+++ sbibm/metrics/__init__.py
@@ -1,5 +1,4 @@
 from sbibm.metrics.c2st import c2st  # noqa: F401
-from sbibm.metrics.ksd import ksd_gaussian_kernel_interface  # noqa: F401
-from sbibm.metrics.mmd import mmd_gaussian_kernel  # noqa: F401
-from sbibm.metrics.mvn_kl import mvn_kl_pq, mvn_kl_qp  # noqa: F401
+from sbibm.metrics.ksd import ksd  # noqa: F401
+from sbibm.metrics.mmd import mmd  # noqa: F401
 from sbibm.metrics.ppc import median_distance  # noqa: F401

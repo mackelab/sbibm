93cfbe0f564eb6567f06bbc88107e8ef6c45be7c
Jan-Matthis <mail@jan-matthis.de>
2020-11-14 12:53:54 +0100

    Rename mmd

--- sbibm/metrics/mmd.py
+++ sbibm/metrics/mmd.py
@@ -13,7 +13,7 @@ from sbibm.utils.torch import get_default_device
 log = logging.getLogger(__name__)
 
 
-def mmd_gaussian_kernel(
+def mmd(
     X: torch.Tensor,
     Y: torch.Tensor,
     implementation: str = "tp_sutherland",
@@ -22,27 +22,12 @@ def mmd_gaussian_kernel(
 ) -> torch.Tensor:
     """Estimate MMD^2 statistic with Gaussian kernel
 
-
-    Currently different implementations are available, in order to validate accuracy and compare speeds.
-
-    The widely used median heuristic for bandwidth-selection of the Gaussian kernel is used.
-
-    Note that there are different options to choose the heuristic:
-
-        1. only use median distance between samples in X (Papamakarios et al., Greenberg et al.)
-        2. use median distances between samples in Z = [X,Y] (Gretton 2012)
-        3. use median distances between samples of X and Y
-
-    We use 1. here, in line with Papamakarios and APT. Gretton 2012 uses option 2.
-    TODO: Decide on scheme.
-
-    Alternatives: Cross-validation, lambda scheme proposed in On the Decreasing Power of Kernel and Distance Based Nonparametric Hypothesis Tests in High Dimensions, mixtures of kernels.
+    Currently different implementations are available, in order to validate accuracy and compare speeds. The widely used median heuristic for bandwidth-selection of the Gaussian kernel is used.
     """
     if torch.isnan(X).any() or torch.isnan(Y).any():
         return torch.tensor(float("nan"))
 
     tic = time.time()  # noqa
-    # log.info(f"MMD implementation: {implementation}")
 
     if z_score:
         X_mean = torch.mean(X, axis=0)
@@ -60,29 +45,6 @@ def mmd_gaussian_kernel(
         sigma_tensor = torch.median(torch.pdist(torch.cat([X, Y])))
     else:
         raise NotImplementedError
-
-    if False:
-        # Computes alternatives heuristics in numpy and torch
-        xs = X.cpu().numpy()
-        ys = Y.cpu().numpy()
-        xx_sq_dists = np.sum(
-            np.array([x1 - x2 for x1, x2 in itertools.combinations(xs, 2)]) ** 2, axis=1
-        )
-        yy_sq_dists = np.sum(
-            np.array([y1 - y2 for y1, y2 in itertools.combinations(ys, 2)]) ** 2, axis=1
-        )
-        xy_sq_dists = np.sum(
-            np.array([x1 - y2 for x1, y2 in itertools.product(xs, ys)]) ** 2, axis=1
-        )
-        scale_1 = np.median(np.sqrt(xx_sq_dists))
-        scale_2 = np.median(
-            np.sqrt(np.concatenate([xx_sq_dists, yy_sq_dists, xy_sq_dists]))
-        )
-        scale_3 = np.median(np.sqrt(np.concatenate([xy_sq_dists])))
-
-        scale_1_torch = torch.median(torch.pdist(X))
-        scale_2_torch = torch.median(torch.pdist(torch.cat([X, Y])))
-        scale_3_torch = tts_pdist(X, Y).median()
 
     # Compute MMD
     if implementation == "tp_sutherland":

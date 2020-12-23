1ee91271f8fbba80157908d410485c1fb668c724
Jan-Matthis <mail@jan-matthis.de>
2020-12-23 18:13:15 +0100

    sbi 0.14.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ REQUIRED = [
     "pandas>=1.0.0",
     "pyabc>=0.10.8",
     "pyabcranger>=0.0.48",
-    "sbi==0.14.1",
+    "sbi==0.14.2",
     "pyro-ppl",
     "scikit-learn",
     "torch>=1.5.1",
